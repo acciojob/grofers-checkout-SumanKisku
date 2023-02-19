9806944547320dd68db87a24844c54f33cb9c985
Suman Kisku <sumankisku1@gmail.com>
2023-02-19 12:07:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+const tbody = document.querySelector("tbody");
+tr.appendChild(td);
+tbody.appendChild(tr);
 
 const getSum = () => {
 //Add your code here
-  
+    let total = 0;
+    const prices = document.querySelectorAll(".price");
+    prices.forEach((price)=> total += Number(price.innerHTML));
+    td.innerText = total;
 };
 
 getSumBtn.addEventListener("click", getSum);
